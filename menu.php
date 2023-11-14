f4fe7a9a87ea5cb7e17da1c1755fa05d048afd4b
anindya hudaa <anindyahudaa@gmail.com>
2023-11-14 15:02:53 +0700

    mengembalikan warna

--- menu.php
+++ menu.php
@@ -16,7 +16,7 @@
         }
 
         .navbar {
-            background-color: #3a9c98 ;
+            background-color: #000000 ;
             padding: 15px 0;
             box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
         }
