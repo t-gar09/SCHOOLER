60b2b75c2d7b7bc50c10b6e40d1f52c5c9ae163f
Ratna sari <nanasari2003@gmail.com>
2023-11-21 16:13:36 +0700

    Navbar home

--- tshop/home.php
+++ tshop/home.php
@@ -58,125 +58,23 @@
                             <div class="main-menu menu-color--white menu-hover-color--white">
                                 <nav>
                                     <ul>
-                                        <li class="has-dropdown">
-                                            <a class="active main-menu-link" href="index.html">Home <i
-                                                    class="fa fa-angle-down"></i></a>
-                                            <!-- Sub Menu -->
-                                            <ul class="sub-menu">
-                                                <li><a href="index.html">Home</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="has-dropdown has-megaitem">
-                                            <a href="product-details-default.html">Shop <i
-                                                    class="fa fa-angle-down"></i></a>
-                                            <!-- Mega Menu -->
-                                            <div class="mega-menu">
-                                                <ul class="mega-menu-inner">
-                                                    <!-- Mega Menu Sub Link -->
-                                                    <li class="mega-menu-item">
-                                                        <a href="#" class="mega-menu-item-title">Shop Layouts</a>
-                                                        <ul class="mega-menu-sub">
-                                                            <li><a href="shop-grid-sidebar-left.html">Grid Left
-                                                                    Sidebar</a></li>
-                                                            <li><a href="shop-grid-sidebar-right.html">Grid Right
-                                                                    Sidebar</a></li>
-                                                            <li><a href="shop-full-width.html">Full Width</a></li>
-                                                            <li><a href="shop-list-sidebar-left.html">List Left
-                                                                    Sidebar</a></li>
-                                                            <li><a href="shop-list-sidebar-right.html">List Right
-                                                                    Sidebar</a></li>
-                                                        </ul>
-                                                    </li>
-                                                    <!-- Mega Menu Sub Link -->
-                                                    <li class="mega-menu-item">
-                                                        <a href="#" class="mega-menu-item-title">Other Pages</a>
-                                                        <ul class="mega-menu-sub">
-                                                            <li><a href="cart.html">Cart</a></li>
-                                                            <li><a href="empty-cart.html">Empty Cart</a></li>
-                                                            <li><a href="wishlist.html">Wishlist</a></li>
-                                                            <li><a href="compare.html">Compare</a></li>
-                                                            <li><a href="checkout.html">Checkout</a></li>
-                                                            <li><a href="login.html">Login</a></li>
-                                                            <li><a href="my-account.html">My Account</a></li>
-                                                        </ul>
-                                                    </li>
-                                                    <!-- Mega Menu Sub Link -->
-                                                    <li class="mega-menu-item">
-                                                        <a href="#" class="mega-menu-item-title">Product Types</a>
-                                                        <ul class="mega-menu-sub">
-                                                            <li><a href="product-details-default.html">Product
-                                                                    Default</a></li>
-                                                            <li><a href="product-details-variable.html">Product
-                                                                    Variable</a></li>
-                                                            <li><a href="product-details-affiliate.html">Product
-                                                                    Referral</a></li>
-                                                            <li><a href="product-details-group.html">Product Group</a>
-                                                            </li>
-                                                            <li><a href="product-details-single-slide.html">Product
-                                                                    Slider</a></li>
-                                                        </ul>
-                                                    </li>
-                                                    <!-- Mega Menu Sub Link -->
-                                                    <li class="mega-menu-item">
-                                                        <a href="#" class="mega-menu-item-title">Product Types</a>
-                                                        <ul class="mega-menu-sub">
-                                                            <li><a href="product-details-tab-left.html">Product Tab
-                                                                    Left</a></li>
-                                                            <li><a href="product-details-tab-right.html">Product Tab
-                                                                    Right</a></li>
-                                                            <li><a href="product-details-gallery-left.html">Product
-                                                                    Gallery Left</a></li>
-                                                            <li><a href="product-details-gallery-right.html">Product
-                                                                    Gallery Right</a></li>
-                                                            <li><a href="product-details-sticky-left.html">Product
-                                                                    Sticky Left</a></li>
-                                                            <li><a href="product-details-sticky-right.html">Product
-                                                                    Sticky right</a></li>
-                                                        </ul>
-                                                    </li>
-                                                </ul>
-                                                <div class="menu-banner">
-                                                    <a href="#" class="menu-banner-link">
-                                                        <img class="menu-banner-img"
-                                                            src="assets/images/banner/menu-banner.jpg" alt="">
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li class="has-dropdown">
-                                            <a href="blog-single-sidebar-left.html">Blog <i
-                                                    class="fa fa-angle-down"></i></a>
-                                            <!-- Sub Menu -->
-                                            <ul class="sub-menu">
-                                                <li><a href="blog-grid-sidebar-left.html">Blog Grid Sidebar left</a>
-                                                </li>
-                                                <li><a href="blog-grid-sidebar-right.html">Blog Grid Sidebar Right</a>
-                                                </li>
-                                                <li><a href="blog-full-width.html">Blog Full Width</a></li>
-                                                <li><a href="blog-list-sidebar-left.html">Blog List Sidebar Left</a>
-                                                </li>
-                                                <li><a href="blog-list-sidebar-right.html">Blog List Sidebar Right</a>
-                                                </li>
-                                                <li><a href="blog-single-sidebar-left.html">Blog Single Sidebar left</a>
-                                                </li>
-                                                <li><a href="blog-single-sidebar-right.html">Blog Single Sidebar
-                                                        Right</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="has-dropdown">
-                                            <a href="#">Pages <i class="fa fa-angle-down"></i></a>
-                                            <!-- Sub Menu -->
-                                            <ul class="sub-menu">
-                                                <li><a href="faq.html">Frequently Questions</a></li>
-                                                <li><a href="privacy-policy.html">Privacy Policy</a></li>
-                                                <li><a href="404.html">404 Page</a></li>
-                                            </ul>
+                                    <li>
+                                            <a href="home.php">Home</a>
                                         </li>
                                         <li>
-                                            <a href="about-us.html">About Us</a>
+                                            <a href="menu.php">Katalog</a>
                                         </li>
                                         <li>
-                                            <a href="contact-us.html">Contact Us</a>
+                                            <a href="about-us.html">Keranjang</a>
+                                        </li>
+                                        <li>
+                                            <a href="login.php">Login</a>
+                                        </li>
+                                        <li>
+                                            <a href="daftar.php">Daftar</a>
+                                        </li>
+                                        <li>
+                                            <a href="checkout.phps">Checkout</a>
                                         </li>
                                     </ul>
                                 </nav>
