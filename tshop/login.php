1ddbf0e36990aed0f7fc894be0012f25754b868e
Tegar Febriansyah <E31221263@student.polije.ac.id>
2023-11-28 23:16:13 +0700

    test

--- tshop/login.php
+++ tshop/login.php
@@ -9,7 +9,7 @@ include'koneksi.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Pelanggan</title>
+    <title>Login test</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
